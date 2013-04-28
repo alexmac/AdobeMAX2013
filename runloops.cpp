71a3becc8c30d2e5425bddbb0969d49422aeb908
Alex Macdonald <alexmac@adobe.com>
2013-04-28 14:24:38 -0700

    Runloops example

---
+++ runloops.cpp
@@ -0,0 +1,56 @@
+//Ex1:
+
+while(gameRunning) {
+	handleInput();
+	renderFrame();
+}
+
+//Ex2:
+
+while(flashPlayerRunning) {
+	handleInput();
+	//Execute AS3 enterFrame
+	{
+		while(gameRunning) { // This blocks outer loop!
+			handleInput();
+			renderFrame();
+		}
+	}
+	renderFrame();
+}
+
+//Ex3:
+
+while(flashPlayerRunning) {
+	handleInput();
+	//Execute AS3 enterFrame
+	{
+		if(gameRunning) {
+			handleInput();
+			renderFrame();
+		}
+	}
+	renderFrame();
+}
+
+//Ex4:
+
+while(flashPlayerRunning) {
+	handleInput();
+	//Execute AS3 enterFrame
+	{
+		CModule.serviceUIRequests()
+	}
+	renderFrame();
+}
+
+while(backgroundWorkerRunning) {
+	//Execute AS3 enterFrame
+	{
+		while(gameRunning) { // This blocks outer loop, but that's OK.
+			handleInput();
+			renderFrame();
+			avm2UIthunk(doStuffOnMainThread);
+		}
+	}
+}
